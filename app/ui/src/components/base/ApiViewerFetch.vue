8762e82d5eaea6fa1395acf88d8ba763cda6a36d
Carlos Soriano <soriano.carlitos@gmail.com>
2023-12-07 07:15:03 -0500

    save

---
+++ app/ui/src/components/base/ApiViewerFetch.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="page-container">
+    <div>
+      <h1>{{ title }}</h1>
+      <Link :href="`/api-data`" :title="'back'">
+      <i class="i-carbon-page-first" inline-block />
+      </Link>
+      <div v-if="!loaded">
+        <h1 class="text-4xl font-bold">Loading...</h1>
+      </div>
+      <pre class="text-left" v-else-if="error">{{ error }}</pre>
+      <div v-else>
+        <JsonTree :data="data" />
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.page-container {
+  display: flex;
+  flex-direction: column;
+}
+</style>
+  
+<script lang="ts">
+import { computed, ref } from 'vue';
+import Counter from '@/ui/src/components/Counter.vue'
+import Link from '@/ui/src/components/base/Link.vue'
+import JsonTree from '@/ui/src/components/base/JsonTree.vue'
+
+export default {
+  components: {
+    Counter,
+    Link,
+    JsonTree,
+  },
+  props: {
+    title: {
+      type: String,
+      required: false,
+    },
+    data: {
+      type: Object,
+      required: false,
+    },
+    loading: {
+      type: Boolean,
+      required: false,
+    },
+    error: {
+      type: Object,
+      required: false,
+    },
+  },
+
+  async setup(props) {
+    const NOOP = {
+      data: undefined,
+      error: undefined,
+      loaded: ref(false),
+    }
+    if (typeof window === "undefined") {
+      return NOOP
+    }
+
+    let loading = ref(props.loading);
+    let error = ref(props.error);
+    let data = ref(props.data);
+
+    const loaded = computed(() => loading.value === false);
+    return { data, loaded, error };
+  },
+}
+</script>
--- app/ui/vite.config.ts
+++ app/ui/vite.config.ts
@@ -8,6 +8,7 @@ import LinkAttributes from 'markdown-it-link-attributes';
 import path from 'node:path';
 import { defineConfig, loadEnv, UserConfig } from 'vite';
 import { InlineConfig } from 'vitest';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { fileURLToPath } from 'node:url';
 // import { nodePolyfills } from "vite-plugin-node-polyfills";
 import dotenv from 'dotenv';
@@ -50,6 +51,7 @@ export default ({ mode }: { mode: string }) => {
       vue({
         include: [/\.vue$/, /\.md$/],
       }),
+      tsconfigPaths(),
       vike(),
       Unocss(),
       Markdown({
@@ -96,12 +98,12 @@ export default ({ mode }: { mode: string }) => {
       target: 'esnext',
     },
 
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('../', import.meta.url)),
-        stream: 'readable-stream',
-      },
-    },
+    // resolve: {
+    //   alias: {
+    //     '@': fileURLToPath(new URL('../', import.meta.url)),
+    //     stream: 'readable-stream',
+    //   },
+    // },
 
     test: vitestConfig,
   });
